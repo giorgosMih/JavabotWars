05b7850d9fa502c979b7db0701a34da333610420
giorgosMih <george19948@gmail.com>
2020-07-09 20:57:48 +0300

    Initial Commit

---
+++ Source/UEtutorial/RWTxtFile.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RWTxtFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UETUTORIAL_API URWTxtFile : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY() public:
+
+		UFUNCTION(BlueprintPure, Category = "Custom", meta = (Keywords = "LoadTxt"))
+			static bool LoadTxt(FString FileNameA, FString& SaveTextA);
+
+		UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "SaveTxt"))
+			static bool SaveTxt(FString SaveTextB, FString FileNameB);
+};
---
+++ Source/UEtutorial/UEtutorial.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UEtutorial.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEtutorial, "UEtutorial" );
